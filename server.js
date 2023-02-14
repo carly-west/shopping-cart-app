594959971670a26d81b1622157a08c8b54f2ecdd
carly-west <carly.west@byui.edu>
2023-02-14 15:03:15 -0700

    added 0auth

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongodb = require("./db/connect");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+const { auth, requiresAuth } = require('express-openid-connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -8,10 +9,11 @@ const app = express();
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use("/", require("./routes"));
+
+  .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
   if (err) {
@@ -20,4 +22,25 @@ mongodb.initDb((err) => {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
+});
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:8080',
+  clientID: 'qo5jv9ISSilSZU2gSALJDurs0yK8q8Ws',
+  issuerBaseURL: 'https://dev-b0rkht1ryuza4xk6.us.auth0.com',
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });

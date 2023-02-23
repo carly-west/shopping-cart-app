441f81dfa701760891e0a38751c5549230d66ebc
carly-west <carly.west@byui.edu>
2023-02-23 09:10:52 -0700

    added auth0

--- routes/shopping-list.js
+++ routes/shopping-list.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { requiresAuth } = require('express-openid-connect');
 
 const contactsController = require('../controllers');
 
-router.get('/', contactsController.viewAllListItems);
+router.get('/', requiresAuth(), contactsController.viewAllListItems);
 
-router.post('/', contactsController.addListItem);
+router.post('/', requiresAuth(), contactsController.addListItem);
 
-router.put('/:id', contactsController.updateListItem);
+router.put('/:id', requiresAuth(), contactsController.updateListItem);
 
-router.delete('/:id', contactsController.deleteListItem);
+router.delete('/:id', requiresAuth(), contactsController.deleteListItem);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,27 @@ const { auth, requiresAuth } = require('express-openid-connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:8080',
+  clientID: 'dIig4BU5sc1YaUY1srd5YnzMZvgng9Rn',
+  issuerBaseURL: 'https://dev-b0rkht1ryuza4xk6.us.auth0.com',
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
@@ -22,25 +43,4 @@ mongodb.initDb((err) => {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
-});
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:8080',
-  clientID: 'qo5jv9ISSilSZU2gSALJDurs0yK8q8Ws',
-  issuerBaseURL: 'https://dev-b0rkht1ryuza4xk6.us.auth0.com',
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
-
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
 });
